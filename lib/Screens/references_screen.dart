daf7989e3975fe57285e1c47a5e274d1c7092e2b
Angelina <asg1997@yandex.ru>
2021-11-06 22:18:01 +0300

    1. Установили все экраны 2. Добавили BottomBar 3. Сделали идентификаторы для навигаций

---
+++ lib/Screens/references_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReferencesScreen extends StatelessWidget {
+  const ReferencesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
