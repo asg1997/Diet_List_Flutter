0b40d9c0bf428b0cbb3d2cfc0cebae46f846acbc
Angelina <asg1997@yandex.ru>
2021-11-07 15:31:25 +0300

    Первы

--- lib/Screens/diets_groups_screen.dart
+++ lib/Screens/diets_groups_screen.dart
@@ -5,7 +5,59 @@ class DietsGroupsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: setAppBar('Что вам нужно?'));
+    return Scaffold(
+      appBar: setAppBar('Что вам нужно?'),
+      body: Container(
+        padding: EdgeInsets.only(top: 12, left: 16, right: 16),
+        child: ListView.builder(
+          itemCount: 2,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              margin: EdgeInsets.only(top: 12),
+              height: 120,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                    image: AssetImage('assets/diet_image.png'),
+                    fit: BoxFit.fill),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 20, color: Colors.black.withOpacity(0.13)),
+                ],
+              ),
+              child: Container(
+                margin:EdgeInsets.only(left: 35, top: 25, bottom: 25, right: 35),
+                color: Colors.red.withOpacity(0.3),
+                child: Column(
+                  children: [
+                    Text('Заболевания', style: TextStyle(
+                      color: Color.fromRGBO(13, 92, 108, 1),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16
+                    ),)
+                  ],
+                ),
+              )
+
+              // child: Container(
+              //   margin:
+              //       EdgeInsets.only(left: 35, top: 25, bottom: 25, right: 35),
+              //   color: Colors.red.withOpacity(0.3),
+              //   child: Text(
+              //     'Заболевания',
+              //     style: TextStyle(
+              //         color: Color.fromRGBO(13, 92, 108, 1),
+              //         fontWeight: FontWeight.bold,
+              //         fontSize: 16),
+              //   ),
+              //
+              // ),
+            );
+          },
+        ),
+      ),
+    );
   }
 
   // Внешний вид Эпп Бара
@@ -24,4 +76,3 @@ class DietsGroupsScreen extends StatelessWidget {
     );
   }
 }
-
