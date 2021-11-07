4f9c7fa8d6a97195590a0c84e8658c02012af777
Yana <asg1997@yandex.ru>
2021-11-07 16:03:13 +0300

    'экран яна'

--- lib/Screens/diets_groups_screen.dart
+++ lib/Screens/diets_groups_screen.dart
@@ -13,47 +13,71 @@ class DietsGroupsScreen extends StatelessWidget {
           itemCount: 2,
           itemBuilder: (BuildContext context, int index) {
             return Container(
-              margin: EdgeInsets.only(top: 12),
-              height: 120,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-                image: DecorationImage(
-                    image: AssetImage('assets/diet_image.png'),
-                    fit: BoxFit.fill),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 20, color: Colors.black.withOpacity(0.13)),
-                ],
-              ),
-              child: Container(
-                margin:EdgeInsets.only(left: 35, top: 25, bottom: 25, right: 35),
-                color: Colors.red.withOpacity(0.3),
-                child: Column(
-                  children: [
-                    Text('Заболевания', style: TextStyle(
-                      color: Color.fromRGBO(13, 92, 108, 1),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16
-                    ),)
+                margin: EdgeInsets.only(top: 12),
+                height: 120,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                      image: AssetImage('assets/diet_image.png'),
+                      fit: BoxFit.fill),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 20, color: Colors.black.withOpacity(0.13)),
                   ],
                 ),
-              )
+                child: Container(
+                  margin:
+                      EdgeInsets.only(left: 35, top: 25, bottom: 25, right: 35),
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Заболевания',
+                          style: TextStyle(
+                              color: Color.fromRGBO(13, 92, 108, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        RichText(
+                            text: TextSpan(
+                                text: 'диеты\n',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(129, 129, 129, 1),
+                                  fontSize: 14,
+                                ),
+                                children: [
+                              TextSpan(
+                                text: 'по заболеваниям',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(129, 129, 129, 1),
+                                  fontSize: 14,
+                                )
+                              )
+                            ]))
+                      ],
+                    ),
+                  ),
+                )
 
-              // child: Container(
-              //   margin:
-              //       EdgeInsets.only(left: 35, top: 25, bottom: 25, right: 35),
-              //   color: Colors.red.withOpacity(0.3),
-              //   child: Text(
-              //     'Заболевания',
-              //     style: TextStyle(
-              //         color: Color.fromRGBO(13, 92, 108, 1),
-              //         fontWeight: FontWeight.bold,
-              //         fontSize: 16),
-              //   ),
-              //
-              // ),
-            );
+                // child: Container(
+                //   margin:
+                //       EdgeInsets.only(left: 35, top: 25, bottom: 25, right: 35),
+                //   color: Colors.red.withOpacity(0.3),
+                //   child: Text(
+                //     'Заболевания',
+                //     style: TextStyle(
+                //         color: Color.fromRGBO(13, 92, 108, 1),
+                //         fontWeight: FontWeight.bold,
+                //         fontSize: 16),
+                //   ),
+                //
+                // ),
+                );
           },
         ),
       ),
