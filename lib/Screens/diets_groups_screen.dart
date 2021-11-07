1bed51e44a9b909ca93b6d698e81b0b063a871f4
Yana <asg1997@yandex.ru>
2021-11-07 19:34:27 +0300

    Сделала переход на экран диет

--- lib/Screens/diets_groups_screen.dart
+++ lib/Screens/diets_groups_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'diets_screen.dart';
 
 class DietsGroupsScreen extends StatelessWidget {
   const DietsGroupsScreen({Key? key}) : super(key: key);
@@ -21,82 +22,71 @@ class DietsGroupsScreen extends StatelessWidget {
 
   Container cell(BuildContext context) {
     return Container(
-      child: SizedBox(
-              height: 120,
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width,
-              child: Stack(
-                children: [
-                  Container(
-                      margin: EdgeInsets.only(top: 12),
-                      height: 120,
-                      width: MediaQuery
-                          .of(context)
-                          .size
-                          .width,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        image: DecorationImage(
-                            image: AssetImage('assets/diet_image.png'),
-                            fit: BoxFit.fill),
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 20,
-                              color: Colors.black.withOpacity(0.13)),
-                        ],
-                      ),
+        child: SizedBox(
+            height: 120,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 12),
+                    height: 120,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      image: DecorationImage(
+                          image: AssetImage('assets/diet_image.png'),
+                          fit: BoxFit.fill),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 20,
+                            color: Colors.black.withOpacity(0.13)),
+                      ],
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.only(
+                          left: 35, top: 25, bottom: 25, right: 35),
                       child: Container(
-                        margin: EdgeInsets.only(
-                            left: 35, top: 25, bottom: 25, right: 35),
-                        child: Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Заболевания',
-                                style: TextStyle(
-                                    color: Color.fromRGBO(13, 92, 108, 1),
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16),
-                              ),
-                              SizedBox(
-                                height: 3,
-                              ),
-                              Text(
-                                'диеты',
-                                style: TextStyle(
-                                    color: Color.fromRGBO(129, 129, 129, 1),
-                                    fontSize: 14),
-                              ),
-                              SizedBox(
-                                height: 3,
-                              ),
-                              Text(
-                                'по заболеваниям',
-                                style: TextStyle(
-                                    color: Color.fromRGBO(129, 129, 129, 1),
-                                    fontSize: 14),
-                              )
-                            ],
-                          ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Заболевания',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(13, 92, 108, 1),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              'диеты',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(129, 129, 129, 1),
+                                  fontSize: 14),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(
+                              'по заболеваниям',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(129, 129, 129, 1),
+                                  fontSize: 14),
+                            )
+                          ],
                         ),
-                      )
-
-                  ),
-                  ListTile(
-                    onTap: () {
-                      print('sdf');
-                    },
-                  )
-                ],
-              )
-          )
-
-    );
+                      ),
+                    )),
+                ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => DietsScreen()));
+                  },
+                )
+              ],
+            )));
   }
-
 
 // Внешний вид Эпп Бара
   AppBar setAppBar(String title) {
