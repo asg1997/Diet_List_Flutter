a93ec597d32a553bc9d628a229a7789e7359311e
Angelina <asg1997@yandex.ru>
2021-11-10 14:40:34 +0300

    ver 2.0.0

---
+++ lib/Screens/links_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LinksScreen extends StatefulWidget {
+  const LinksScreen({Key? key}) : super(key: key);
+
+  @override
+  _LinksScreenState createState() => _LinksScreenState();
+}
+
+class _LinksScreenState extends State<LinksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Screens/search_diet_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchDietScreen extends StatefulWidget {
+  const SearchDietScreen({Key? key}) : super(key: key);
+
+  @override
+  _SearchDietScreenState createState() => _SearchDietScreenState();
+}
+
+class _SearchDietScreenState extends State<SearchDietScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
