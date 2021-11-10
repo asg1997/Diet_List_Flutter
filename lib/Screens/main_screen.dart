2cc212dfd1dba5dda6c30c1927926538d1bc74b4
Angelina <asg1997@yandex.ru>
2021-11-10 19:14:57 +0300

    Шапка главного экрана

--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -10,6 +10,40 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/osnovnoi.png')
+
+                  )
+                ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                  Text('Гастрит'),
+                  Icon(Icons.keyboard_arrow_down_rounded)
+                ]),
+              ),
+
+            ),
+            Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 25),
+                  child: Icon(Icons.list),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Screens/search_diet_screen.dart
+++ lib/Screens/search_diet_screen.dart
@@ -9,6 +9,10 @@ class SearchDietScreen extends StatefulWidget {
 
 class _SearchDietScreenState extends State<SearchDietScreen> {
   @override
+
+  String _mySelection = '';
+  List<String> dietsList = ['Аритмия', 'Гастрит'];
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -20,22 +24,25 @@ class _SearchDietScreenState extends State<SearchDietScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [Image.asset('assets/pribori.png')],
               ),
+              addDropDownList(context),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [ SizedBox(
-                  height: 61,
-                  width: 174,
-                  child: ElevatedButton(onPressed: () {
-                    Navigator.pushNamed(context, '/MainScreen');
-                  },
-                      style: ElevatedButton.styleFrom(
-                        primary: Color.fromRGBO(255, 168, 0, 1),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(210),
+                children: [
+                  SizedBox(
+                    height: 61,
+                    width: 174,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/MainScreen');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(255, 168, 0, 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(210),
+                          ),
                         ),
-                      ),
-                      child: Text('Далее')),
-                )
+                        child: Text('Далее')),
+                  )
                 ],
               )
             ],
@@ -49,4 +56,34 @@ class _SearchDietScreenState extends State<SearchDietScreen> {
       ),
     );
   }
+
+  Widget addDropDownList(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25, right: 25),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Expanded(
+
+          //     child: DropdownButtonHideUnderline(
+          //   child: ButtonTheme(
+          //     alignedDropdown: true,
+          //     child: DropdownButton(
+          //       value: _mySelection,
+          //       onChanged: (newValue) {
+          //
+          //       },
+          //       items: dietsList.map<DropdownMenuItem>,
+          //   ),
+          // ))
+        ],
+      ),
+    );
+  }
+
+
 }
