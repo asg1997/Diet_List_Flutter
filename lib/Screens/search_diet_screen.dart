5c21a546d36ed64ab5c2cb2c8ca0ba537f82dee3
Yana <asg1997@yandex.ru>
2021-11-10 16:20:23 +0300

    Яна

--- lib/Screens/search_diet_screen.dart
+++ lib/Screens/search_diet_screen.dart
@@ -10,6 +10,51 @@ class SearchDietScreen extends StatefulWidget {
 class _SearchDietScreenState extends State<SearchDietScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Image.asset('assets/pribori.png')],
+              ),
+              Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        hintText: "Заболевание",
+                        labelText: 'Выбрать заболевание',
+                        labelStyle:
+                        TextStyle(
+                            fontSize: 24,
+                            color: Colors.black),
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    maxLines: 3,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text('Далее')),
+                ],
+              )
+            ],
+          ),
+        ),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/osnovnoi.png'),
+                // растягиваем фото
+                fit: BoxFit.fill)),
+      ),
+    );
   }
 }
