8f4c19a7b710ab75ba1f08fc913c2f7b7f3fcc9e
Angelina <asg1997@yandex.ru>
2021-11-12 19:06:18 +0300

    рлолл

---
+++ lib/Components/custom_app_bar.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends AppBar {
+
+
+  AppBar create(String title) {
+    return AppBar(
+      title: Text(title,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+        ),
+      ),
+      backgroundColor: Colors.black.withOpacity(0),
+      shadowColor: Colors.black.withOpacity(0),
+    );
+  }
+}
+//
+// class CustomAppBar extends StatefulWidget {
+//   CustomAppBar({Key? key,
+//
+//   this.hasDropdown = false,
+//     this.hasMenu = false,
+//     this.title = 'Гастрит'
+//
+//   }) : super(key: key);
+//
+//   bool hasMenu;
+//   String title;
+//   bool hasDropdown;
+//
+//
+//   @override
+//   _CustomAppBarState createState() => _CustomAppBarState();
+// }
+//
+// class _CustomAppBarState extends State<CustomAppBar> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       title: Text(widget.title,
+//         style: TextStyle(
+//           color: Colors.black,
+//         ),
+//       ),
+//       backgroundColor: Colors.black.withOpacity(0),
+//       shadowColor: Colors.black.withOpacity(0),
+//     );
+//   }
+// }
--- lib/Screens/main_screen.dart
+++ lib/Screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:diet_list_flutter/Components/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -15,191 +16,188 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            // Задняя картинка
-            Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/fon.png'),
-                      fit: BoxFit.fitWidth,
-                      alignment: Alignment.topCenter)),
-            ),
-            // Заголовок
-            Align(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child:
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.transparent,
-                          shadowColor: Colors.transparent),
-                      child: Row(children: [
-                        Text(
-                          'Гастрит',
-                          style: TextStyle(
-                              fontSize: 22,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Padding(padding: EdgeInsets.only(left: 5)),
-                        Icon(
-                          Icons.keyboard_arrow_down_rounded,
-                          size: 25,
-                          color: Color.fromRGBO(255, 168, 0, 1),
-                        )
-                      ])),
-                ]),
+      extendBodyBehindAppBar: true,
+      appBar: CustomAppBar().create('Гастрит'),
+      body: Stack(
+        children: [
+          // Картинка на заднем фоне
+        Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/fon.png'),
+                        fit: BoxFit.fitWidth,
+                        alignment: Alignment.topCenter)),
+              ),
+          // Основная информация
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 10,
+              left: 20,
+              right: 20),
+              child: Column(
+                children: [
+                  discriptionTile(),
+                  SizedBox(height: 10),
+                  discriptionTile(),
+                ],
               ),
             ),
-            // Меню
-            Align(
-                alignment: Alignment.topLeft,
-                child: Padding(
-                    padding: const EdgeInsets.only(top: 19, left: 5),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.transparent,
-                          shadowColor: Colors.transparent),
-                      child: Icon(
-                        Icons.menu_outlined,
-                        size: 30,
-                        color: Colors.white,
-                      ),
-                    ))),
-            descriptionPanel()
-          ],
-        ),
+          ),
+
+        ],
+
       ),
+      // body: SafeArea(
+      //   child: Stack(
+      //     children: [
+      //       // MARK: - Шапка
+      //       // Задняя картинка
+      //       Container(
+      //         decoration: BoxDecoration(
+      //             image: DecorationImage(
+      //                 image: AssetImage('assets/fon.png'),
+      //                 fit: BoxFit.fitWidth,
+      //                 alignment: Alignment.topCenter)),
+      //       ),
+      //       // Заголовок
+      //       Align(
+      //         alignment: Alignment.topCenter,
+      //         child: Padding(
+      //           padding: const EdgeInsets.only(top: 20),
+      //           child:
+      //               Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      //             ElevatedButton(
+      //                 onPressed: () {},
+      //                 style: ElevatedButton.styleFrom(
+      //                     primary: Colors.transparent,
+      //                     shadowColor: Colors.transparent),
+      //                 child: Row(children: [
+      //                   Text(
+      //                     'Гастрит',
+      //                     style: TextStyle(
+      //                         fontSize: 22,
+      //                         color: Colors.white,
+      //                         fontWeight: FontWeight.bold),
+      //                   ),
+      //                   Padding(padding: EdgeInsets.only(left: 5)),
+      //                   Icon(
+      //                     Icons.keyboard_arrow_down_rounded,
+      //                     size: 25,
+      //                     color: Color.fromRGBO(255, 168, 0, 1),
+      //                   )
+      //                 ])),
+      //           ]),
+      //         ),
+      //       ),
+      //       // Меню
+      //       Align(
+      //           alignment: Alignment.topLeft,
+      //           child: Padding(
+      //               padding: const EdgeInsets.only(top: 19, left: 5),
+      //               child: ElevatedButton(
+      //                 onPressed: () {},
+      //                 style: ElevatedButton.styleFrom(
+      //                     primary: Colors.transparent,
+      //                     shadowColor: Colors.transparent),
+      //                 child: Icon(
+      //                   Icons.menu_outlined,
+      //                   size: 30,
+      //                   color: Colors.white,
+      //                 ),
+      //               ))),
+      //
+      //
+      //       // Ячейки с описанием
+      //       // SizedBox(height: 10,),
+      //       // Column(
+      //       // children: [
+      //       //   discriptionTile(),
+      //       //   SizedBox(height: 10),
+      //       //   discriptionTile(),
+      //       // ],
+      //       // )
+      //
+      //     ],
+      //   ),
+      // ),
     );
   }
 
-  // Widget descriptionPanel() {
-  //   return Container(
-  //     child: ListView.builder(
-  //
-  //         itemCount: list.length,
-  //         padding: EdgeInsets.only(top: 70, left: 24, right: 24),
-  //         itemBuilder: (context, index) => DecoratedBox(
-  //               decoration: BoxDecoration(
-  //                 color: Colors.white,
-  //                 borderRadius: BorderRadius.circular(10),
-  //               ),
-  //               child: ExpansionTile(
-  //                 title: Text(
-  //                   'Описание диеты',
-  //                   style: TextStyle(
-  //                       fontSize: 15,
-  //                       fontWeight: FontWeight.bold,
-  //                       color: Color.fromRGBO(13, 92, 108, 1)),
-  //                 ),
-  //                 children: list
-  //                     .map((value) => DecoratedBox(
-  //                           decoration: BoxDecoration(color: Colors.white),
-  //                           child: ListTile(
-  //                             title: Text(value),
-  //                           ),
-  //                         ))
-  //                     .toList(),
-  //               ),
-  //             )),
-  //   );
-  // }
 
-  Widget descriptionPanel() {
-    return GestureDetector(
-      onTap: () {
-        print('dsg');
-      },
-      child: Column(
+
+  // TODO: - Реализовать нажатие
+  Widget discriptionTile() {
+    return Container(
+      height: 113,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset.zero,
+            color: Colors.black.withOpacity(0.13),
+            spreadRadius: 10,
+            blurRadius: 20,
+          )
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-            Container(
-              height: 113,
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.only(left: 25, right: 50, top: 20, bottom: 18),
-              margin: EdgeInsets.only(top: 81, left: 25, right: 25),
-
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                  offset: Offset.zero,
-                  color: Colors.black.withOpacity(0.13),
-                    spreadRadius: 10,
-                    blurRadius: 20,
-                ),
-                ]
-
-              ),
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Описание диеты',
+          // Текст
+          Expanded(
+            // Левая часть ячейки
+            child: Container(
+              padding: EdgeInsets.only(top: 20, left: 26, bottom: 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Заголовок
+                  Text(
+                    'Описание диеты',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(13, 92, 108, 1)),
+                  ),
+                  // Расстояние между заголовком и описанием
+                  SizedBox(height: 3,),
+                  // Описание
+                  Expanded(
+                    child: Text(
+                      'Performing hot reloadPerforming hot reloadPerforming hot reloadPerforming hot reloadreloadPerforming hot reloadreloadPerforming hot reload ',
                       style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(13, 92, 108, 1)
+                        height: 1.7,
+                        color: Colors.grey,
+                        fontSize: 14,
                       ),
+                      overflow: TextOverflow.clip,
                     ),
-                    SizedBox(height: 7),
-                    Flexible(
-                      child: Text('Нельзя жирное соленое перченное жить все нельзя никак недбчх апрвпрпарп перченное жить все нельзя никак недбчх апрвпрпарп',
-                        style: TextStyle(
-                            fontSize: 14,
-                            color: Color.fromRGBO(129, 129, 129, 1),
-                          height: 1.5,
-                          overflow: TextOverflow.fade,
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.touch_app_outlined,
-                            size: 20,
-                              color: Colors.deepOrange,
-                            )
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              )
-            )
+                  ),
+                  // Растояние от описания до конца ячейки
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              // Ряд с иконкой
+            ),
+          ),
+
+          // Правая часть: Иконка
+          Padding(
+            padding: const EdgeInsets.only(right: 17, top: 16, left: 16),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Icon(Icons.touch_app_outlined,
+                size: 25,
+                color: Color.fromRGBO(255, 168, 0, 1),),
+            ),
+          )
         ],
       ),
     );
   }
 }
-/*child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Описание диеты',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(13, 92, 108, 1)
-                    )
-                      ,),
-                    SizedBox(height: 7),
-                    Flexible(
-                      child: Text('варпоылроылдлрпрлдоллраллодлрлалолровлораоырпоарылдлрпрлдоллраллодлрлалолровлораылдлрпрлдоллраллодлрлалолровлораылдлрпрлдоллраллодлрлалолровлора',
-                        style: TextStyle(
-                        fontSize: 14,
-                        color: Color.fromRGBO(129, 129, 129, 1),
-                        height: 1.5,
-                        overflow: TextOverflow.fade,
-                      ),
-                      ),
-                    ),
-                  ],
-                ),*/
